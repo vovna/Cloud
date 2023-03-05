b1187b76a693b586f65c7315be409b6389afe130
Elena Vovna <elena.vovna@onde.app>
2023-03-05 20:54:35 +0300

    create project

---
+++ ClodTest/ClodTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClodTest
+//
+//  Created by Elena Vovna on 5.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
